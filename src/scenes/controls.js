6e9cc967471d33c47f29fda556ad28c033f8a8e0
nightingale00 <82423327+nightingale00@users.noreply.github.com>
2021-05-11 09:25:45 -0700

    Update controls.js

--- src/scenes/controls.js
+++ src/scenes/controls.js
@@ -4,6 +4,7 @@ class controls extends Phaser.Scene {
   }
 
   create () {
+    //when we have the controls we can change the text below 
     this.controlsText = this.add.text(0, 0, 'Controls', { fontSize: '32px', fill: '#fff' });
     this.zone = this.add.zone(config.width/2, config.height/2, config.width, config.height);
 
