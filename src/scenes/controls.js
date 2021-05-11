93064b2af4f0f4d448eca977af41198501b5f77d
nightingale00 <82423327+nightingale00@users.noreply.github.com>
2021-05-11 09:24:24 -0700

    Add files via upload

---
+++ src/scenes/controls.js
@@ -0,0 +1,15 @@
+class controls extends Phaser.Scene {
+  constructor () {
+    super('controls');
+  }
+
+  create () {
+    this.controlsText = this.add.text(0, 0, 'Controls', { fontSize: '32px', fill: '#fff' });
+    this.zone = this.add.zone(config.width/2, config.height/2, config.width, config.height);
+
+    Phaser.Display.Align.In.Center(
+      this.controlsText,
+      this.zone
+    );
+  }
+};
