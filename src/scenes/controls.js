ec8b1f50103c2648afa391e23387382e686bb6a1
Kyle Worcester-Moore <25kylworc@gmail.com>
2021-05-13 12:55:31 -0700

    fix main.js

--- src/scenes/controls.js
+++ src/scenes/controls.js
@@ -1,10 +1,10 @@
-class controls extends Phaser.Scene {
+class ControlsScene extends Phaser.Scene {
   constructor () {
     super('controls');
   }
 
   create () {
-    //when we have the controls we can change the text below 
+    //when we have the controls we can change the text below
     this.controlsText = this.add.text(0, 0, 'Controls', { fontSize: '32px', fill: '#fff' });
     this.zone = this.add.zone(config.width/2, config.height/2, config.width, config.height);
 
